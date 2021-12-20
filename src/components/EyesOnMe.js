fc3c289b7682d95f3b78301dbf917f9f9b19b1b0
Dakota Castle <barrettcastle@gmail.com>
2021-12-20 13:30:49 -0500

    commit tree 6f41ea5e93de2912cc88524f1941a50202eac636

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function Blur() {
+    console.log("Hey! Eyes on me!");
+  }
+  
+  function Focus() {
+    console.log("Good!");
+  }
+  
+  function EyesOnMe() {
+    return (
+      <div>
+        <button onFocus={Focus} onBlur={Blur}>
+          {" "}
+          'Eyes on me'
+        </button>
+      </div>
+    );
+  }
+  
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleChange(event) {
+      console.log(`${event.target.name}`);
+    }
+  
+    return (
+      <div>
+        <input
+          type="password"
+          name="Entering password..."
+          onChange={handleChange}
+          placeholder="Password"
+        />
+      </div>
+    );
+  }
+  export default Keypad;
+
+
